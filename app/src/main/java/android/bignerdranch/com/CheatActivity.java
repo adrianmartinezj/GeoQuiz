180276e83919aae4f8589cb5a2bf7fec9293906c
Adrian Martinez <adrianjustinmartinez@gmail.com>
2019-02-07 13:57:21 -0800

    Implementing cheat count limit

--- app/src/main/java/android/bignerdranch/com/CheatActivity.java
+++ app/src/main/java/android/bignerdranch/com/CheatActivity.java
@@ -12,6 +12,7 @@ import android.view.View;
 import android.view.ViewAnimationUtils;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 @TargetApi(Build.VERSION_CODES.LOLLIPOP)
 public class CheatActivity extends AppCompatActivity {
@@ -22,10 +23,14 @@ public class CheatActivity extends AppCompatActivity {
     private static final String EXTRA_ANSWER_SHOWN =
             "com.bignerdranch.android.geoquiz.answer_shown";
 
+    private static final String CHEAT_COUNT =
+            "com.bignerdranch.android.geoquiz.cheater_used";
+
     private static final String API_LEVEL =
              Integer.toString(Build.VERSION.SDK_INT);
 
     private boolean mAnswerIsTrue;
+    private int mCheatCount;
 
     private TextView mAnswerTextView;
     private TextView mAPITextView;
@@ -42,6 +47,10 @@ public class CheatActivity extends AppCompatActivity {
         return result.getBooleanExtra(EXTRA_ANSWER_SHOWN, false);
     }
 
+    public static int cheatCount(Intent result){
+        return result.getIntExtra(CHEAT_COUNT, 3);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -51,45 +60,50 @@ public class CheatActivity extends AppCompatActivity {
 
         mAnswerIsTrue = getIntent().getBooleanExtra(EXTRA_ANSWER_IS_TRUE, false);
 
+        mCheatCount = getIntent().getIntExtra(CHEAT_COUNT, 3);
+
         mAnswerTextView = (TextView) findViewById(R.id.answer_text_view);
 
         mShowAnswerButton = (Button) findViewById(R.id.show_answer_button);
         mShowAnswerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (mAnswerIsTrue) {
-                    mAnswerTextView.setText(R.string.true_button);
+                if (mCheatCount > 0) {
+                    if (mAnswerIsTrue) {
+                        mAnswerTextView.setText(R.string.true_button);
+                    } else {
+                        mAnswerTextView.setText(R.string.false_button);
+                    }
+                    setAnswerShownResult(true);
+                    mCheatCount--;
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                        int cx = mShowAnswerButton.getWidth() / 2;
+                        int cy = mShowAnswerButton.getHeight() / 2;
+                        float radius = mShowAnswerButton.getWidth();
+                        Animator anim = ViewAnimationUtils
+                                .createCircularReveal(mShowAnswerButton, cx, cy, radius, 0);
+                        anim.addListener(new AnimatorListenerAdapter() {
+                            @Override
+                            public void onAnimationEnd(Animator animation) {
+                                super.onAnimationEnd(animation);
+                                mShowAnswerButton.setVisibility(View.INVISIBLE);
+                            }
+                        });
+                        anim.start();
+                    } else {
+                        mShowAnswerButton.setVisibility(View.INVISIBLE);
+                    }
                 } else {
-                    mAnswerTextView.setText(R.string.false_button);
-                }
-                setAnswerShownResult(true);
-
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-                    int cx = mShowAnswerButton.getWidth() / 2;
-                    int cy = mShowAnswerButton.getHeight() / 2;
-                    float radius = mShowAnswerButton.getWidth();
-                    Animator anim = ViewAnimationUtils
-                            .createCircularReveal(mShowAnswerButton, cx, cy, radius, 0);
-                    anim.addListener(new AnimatorListenerAdapter() {
-                        @Override
-                        public void onAnimationEnd(Animator animation) {
-                            super.onAnimationEnd(animation);
-                            mShowAnswerButton.setVisibility(View.INVISIBLE);
-                        }
-                    });
-                    anim.start();
-                } else {
-                    mShowAnswerButton.setVisibility(View.INVISIBLE);
+                    Toast.makeText(CheatActivity.this, "You've used up all your cheats!", Toast.LENGTH_SHORT).show();
                 }
             }
         });
-
         mAPITextView.setText(mAPITextView.getText() + API_LEVEL);
     }
 
     private void setAnswerShownResult(boolean isAnswerShown) {
         Intent data = new Intent();
-        data.putExtra(EXTRA_ANSWER_SHOWN, isAnswerShown);
+        (data.putExtra(EXTRA_ANSWER_SHOWN, isAnswerShown)).putExtra(CHEAT_COUNT, mCheatCount);
         setResult(RESULT_OK,   data);
     }
 }
